9f5c3e0d9d0240465c88d7bfd4d92179d8c6da31
lehoangphuc1602 <86216171+lehoangphuc1602@users.noreply.github.com>
2023-01-12 15:08:09 +0900

    revoke all file

--- portal_project/accounts/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import GetUsersView, GetUser
-
-urlpatterns = [
-    path("users", GetUsersView.as_view()),
-    # path("user_profile", GetUser.as_view()),
-]
