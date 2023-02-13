c7d4b8102c8676066398ef0ab12f5701da7b0aa6
MasKatx <tky2102137@stu.o-hara.ac.jp>
2023-02-13 12:09:00 +0900

    change setting to dp

--- portal_project/portal_project/settings.py
+++ portal_project/portal_project/settings.py
@@ -133,14 +133,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = "/static/"
+STATIC_URL = "/build/"
 
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "build/static")]
 
 MEDIA_ROOT = "/usr/share/nginx/html/media"
 MEDIA_URL = "/media/"
-STATIC_ROOT = "/usr/share/nginx/html/static"
+STATIC_ROOT = "/usr/share/nginx/html/build"
 
 REST_FRAMEWORK = {
     "DEFAULT_PERMISSION_CLASSES": [
